2b3fd0e12b9de756d236bf6a65cd1d1b2fc443d0
lowlightXV <moneyngurn2@gmail.com>
2022-06-06 12:59:24 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace arraylist2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            arrayList = new ArrayList();
+        }
+
+        
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string inputList = textBox.Text;
+            arrayList.Add(inputList);
+        }
+
+        private void removeButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputList1 = textBox.Text;
+            arrayList.Remove(inputList1);
+        }
+
+        private void showButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(string data in arrayList) 
+            {
+                textBox.AppendText(data);
+
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
